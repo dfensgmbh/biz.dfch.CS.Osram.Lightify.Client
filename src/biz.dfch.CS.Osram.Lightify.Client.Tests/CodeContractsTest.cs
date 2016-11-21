ff6b509a2792505ac00bc097c295909ae8c7007f
stevenpi <steven.pilatschek@gmail.com>
2016-11-21 11:45:28 +0100

    Project setup

---
+++ src/biz.dfch.CS.Osram.Lightify.Client.Tests/CodeContractsTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using biz.dfch.CS.Testing.Attributes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace biz.dfch.CS.Osram.Lightify.Client.Tests
+{
+    [TestClass]
+    public class CodeContractsTest
+    {
+        [TestMethod]
+        public void InvokeWithTrueSucceeds()
+        {
+            var sut = new CodeContracts();
+            sut.BoolMethod(true);
+        }
+
+        [TestMethod]
+        [ExpectContractFailure(MessagePattern = "Precondition.+itMustBeTrue")]
+        public void InvokeWithFalseThrowsContractException()
+        {
+            var sut = new CodeContracts();
+            sut.BoolMethod(false);
+        }
+
+        [TestMethod]
+        public void InvokeWithStringSucceeds()
+        {
+            var sut = new CodeContracts();
+            sut.StringMethod("Arbitrary String");
+        }
+
+        [TestMethod]
+        [ExpectContractFailure(MessagePattern = "Precondition.+itMustNotBeNullOrEmptyOrWhiteSpace")]
+        public void InvokeWithEmptyStringThrowsContractException()
+        {
+            var sut = new CodeContracts();
+            sut.StringMethod(string.Empty);
+        }
+
+        [TestMethod]
+        [ExpectContractFailure(MessagePattern = "Precondition.+itMustNotBeNullOrEmptyOrWhiteSpace")]
+        public void InvokeWithWhiteSpaceStringThrowsContractException()
+        {
+            var sut = new CodeContracts();
+            sut.StringMethod("   ");
+        }
+
+        [TestMethod]
+        [ExpectContractFailure(MessagePattern = "Precondition.+itMustNotBeNullOrEmptyOrWhiteSpace")]
+        public void InvokeWithNullStringThrowsContractException()
+        {
+            var sut = new CodeContracts();
+            sut.StringMethod(default(string));
+        }
+    }
+}
---
+++ src/biz.dfch.CS.Osram.Lightify.Client/CodeContracts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biz.dfch.CS.Osram.Lightify.Client
+{
+    public class CodeContracts
+    {
+        public void BoolMethod(bool itMustBeTrue)
+        {
+            Contract.Requires(itMustBeTrue);
+        }
+
+        public void StringMethod(string itMustNotBeNullOrEmptyOrWhiteSpace)
+        {
+            Contract.Requires(!string.IsNullOrWhiteSpace(itMustNotBeNullOrEmptyOrWhiteSpace));
+        }
+    }
+}
